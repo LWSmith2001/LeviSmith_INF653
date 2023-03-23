a74015d319273189f1753d5e471234b1d4a3dc52
LWSmith2001 <112495444+LWSmith2001@users.noreply.github.com>
2023-03-23 17:43:50 -0500

    Update database.php

--- midtermproject/model/database.php
+++ midtermproject/model/database.php
@@ -1,5 +1,5 @@
 <?php 
-$dsn = "mysql:host=localhost; dbname=zippyusedautos;";
+$dsn = "mysql:host=r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=mysql:__w87kq1f1n6zgdks8:iyhtwyg9vwzr8cs7@r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306_wqyssctlqst17el3;";
 $username = 'root';
 
 try {
