c4c231f8b360391fdde0bc01b8c234415933d2ce
LWSmith2001 <112495444+LWSmith2001@users.noreply.github.com>
2023-03-23 17:49:20 -0500

    Update database.php

--- midtermproject/model/database.php
+++ midtermproject/model/database.php
@@ -1,9 +1,10 @@
 <?php 
-$dsn = "mysql:host=r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=mysql:__w87kq1f1n6zgdks8:iyhtwyg9vwzr8cs7@r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306_wqyssctlqst17el3;";
-$username = 'root';
+$dsn = "mysql:host=r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=wqyssctlqst17el3;";
+$username = 'w87kq1f1n6zgdks8';
+$password = 'iyhtwyg9vwzr8cs7';
 
 try {
-    $db = new PDO($dsn,$username);
+    $db = new PDO($dsn,$username,$password);
 } catch (PDOException $e) {
     $error_message = "Database Error: ";
     $error_message .= $e->getMessage();
